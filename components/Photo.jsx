7d6eb9e6e522237ceb9313c30c4877295a33c51f
Muhammad Bilal <billo34988@gmail.com>
2024-09-02 23:47:30 +0500

    stats added

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -8,20 +8,26 @@ const Photo = () => {
     <div className="w-full h-full relative">
     
       <motion.div
-        initial={{opacity: 0 }}
+        initial={{ opacity: 0 }}
         animate={{
-            opacity:1,
-            transition: {delay:2 , duration: 0.4, ease:"easeIn"},
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
         }}
       >
         {/* Image */}
         <motion.div
-        initial={{ opacity:0}}
-        animate={{
+          initial={{ opacity: 0 }}
+          animate={{
             opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease:"easeInOut"},
-        }}
-        className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498] mix-blend-lighten absolute">
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[250px] h-[250px] xl:w-[450px] xl:h-[450px] mix-blend-lighten absolute"
+          style={{
+            top: "50%",     // Move towards the bottom of the circle
+            left: "50%",    // Center horizontally
+            transform: "translate(-48%, -53%)", // Adjust positioning
+          }}
+        >
           <Image
             src="/assets/photo.png"
             priority
@@ -32,32 +38,32 @@ const Photo = () => {
           />
         </motion.div>
 
-      {/* circle */}
-      <motion.svg
-      className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-      fill="transparent"
-      viewBox="0 0 506 506"
-      xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.circle
-        cx='253'
-        cy='253'
-        r='250'
-        stroke="#00ff99"
-        strokeWidth="4"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        initial={{ strokeDasharray: "24 10 0 0"}}
-        animate={{
-            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"]
-        }}
-        transition={{
-            duration: 20, 
-            repeat: Infinity,
-            repeatType: "reverse"
-        }}
-        />
-      </motion.svg>
+        {/* circle */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
 
     </div>
